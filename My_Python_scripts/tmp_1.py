e803890c0f012a16ff482a2ef159a24e92c2689a
songweizhi <songwz03@gmail.com>
2020-05-05 13:10:21 +1000

    -1.21.3

--- My_Python_scripts/tmp_1.py
+++ My_Python_scripts/tmp_1.py
@@ -1,28 +1,15 @@
 
-#
-#
+file_in  = '/Users/songweizhi/Desktop/combined_R1.fasta'
+file_out = '/Users/songweizhi/Desktop/combined_R1_formatted.fasta'
+read_strand = '1'
 
+file_out_handle = open(file_out, 'w')
+for each in open(file_in):
 
-drep99_bin_list = []
-for each in open('/Users/songweizhi/Desktop/bins.txt'):
+    if each.startswith('>'):
+        each_split = each.strip().split('_')
+        file_out_handle.write('>read.%s/%s\n' % (each_split[0][2:], read_strand))
+    else:
+        file_out_handle.write(each)
 
-    drep99_bin_list.append(each.strip())
-
-print(drep99_bin_list)
-print(len(drep99_bin_list))
-
-n = 0
-for each in open('/Users/songweizhi/Desktop/Tara_NM_CheckM_0.4_0.05.csv'):
-    each_split = each.strip().split(',')
-
-
-
-    if each_split[0] in drep99_bin_list:
-
-
-
-        print(each_split)
-
-        n += 1
-
-print(n)
+file_out_handle.close()
