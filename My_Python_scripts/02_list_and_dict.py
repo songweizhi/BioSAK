c566e04602224e538b2db29a2e95e562b8a8b4c3
songweizhi <songwz03@gmail.com>
2020-09-18 07:57:53 +1000

    -

--- My_Python_scripts/02_list_and_dict.py
+++ My_Python_scripts/02_list_and_dict.py
@@ -17,24 +17,32 @@ print(unique_list_elements([1,2,3,3,3,3,3,4,5, 'a', 'a']))
 # [1, 2, 3, 4, 5, 'a']
 
 
-
 #################### get_intersection ####################
 
 a = [1,2,3,4,'a',5,6]
 b = [3,4,5,6,'a', 'b',7,8]
 c = set(a).intersection(b)
 print(c)
+# {3, 4, 5, 6, 'a'}
 
+
+#################### enumerate ####################
+
+for i, j in enumerate(['a', 'b', 'c', 'd']):
+    print(i, j)
+# 0 a
+# 1 b
+# 2 c
+# 3 d
 
 
 #################### for loop in one line ####################
 
 colors = ['black', 'white']
 sizes = ['S', 'M', 'L']
-t_shirts = [(color, size) for color in colors
-                          for size in sizes]
-
+t_shirts = [(color, size) for color in colors for size in sizes]
 print(t_shirts)
+# [('black', 'S'), ('black', 'M'), ('black', 'L'), ('white', 'S'), ('white', 'M'), ('white', 'L')]
 
 
 ######################################################### dict #########################################################
@@ -42,17 +50,16 @@ print(t_shirts)
 #################### merge_two_dict ####################
 
 def merge_two_dict(dict_a, dict_b):
-
     dict_c = dict_a.copy()
     dict_c.update(dict_b)
-
     return dict_c
 
 
-dict_a = {'A':'aa', 'B':'bb'}
-dict_b = {'C':'cc', 'D':'dd'}
+dict_a = {'A': 'aa', 'B': 'bb'}
+dict_b = {'C': 'cc', 'D': 'dd'}
 dict_c = merge_two_dict(dict_a, dict_b)
 print(dict_c)
 # {'A': 'aa', 'B': 'bb', 'C': 'cc', 'D': 'dd'}
+
 
 
