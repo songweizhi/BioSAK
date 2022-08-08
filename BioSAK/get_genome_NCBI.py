30032454f9a9b420f3ca68dffca35f2c3b5cb49e
Weizhi Song <songwz03@gmail.com>
2022-08-08 11:23:23 +1000

    -

--- BioSAK/get_genome_NCBI.py
+++ BioSAK/get_genome_NCBI.py
@@ -6,46 +6,49 @@ import multiprocessing as mp
 
 
 get_genome_NCBI_parser_usage = '''
-================================== get_genome_NCBI example commands ==================================
+===================================== get_genome_NCBI example commands =====================================
 
 # Usage:
-1. Go to https://www.ncbi.nlm.nih.gov/genome/browse#!/prokaryotes/refseq_category:reference
-2. Search the taxonomy of genomes you want to download (e.g. prokaryotes, proteobacteria)
-3. Click "Download" on the right side
-4. provide the downloaded csv file to BioSAK with '-csv'
+Before you start, you need to download https://ftp.ncbi.nlm.nih.gov/genomes/GENOME_REPORTS/prokaryotes.txt,
+and provide it to get_genome_NCBI with '-db'.
 
-# Example commands
-BioSAK get_genome_NCBI -csv prokaryotes.csv -out gnm_dir -fna -name -t 8
-BioSAK get_genome_NCBI -csv prokaryotes.csv -out gnm_dir -fna -faa -name -t 8
-BioSAK get_genome_NCBI -csv prokaryotes.csv -id ids.txt -out gnm_dir -fna -gbff -name -t 8 
+# Download all genomes in prokaryotes.csv
+BioSAK get_genome_NCBI -db prokaryotes.csv -out gnm_dir -fna -name -t 8
 
-# id file format (the 6th col of the downloaded csv file, one id per line)
+# Only download genomes in gnm_to_download.txt
+BioSAK get_genome_NCBI -db prokaryotes.csv -out gnm_dir -fna -name -t 8 -id gnm_to_download.txt
+
+# Format of genome id file (specify with "-id")
+# Genome ID can be found in the "Assembly Accession" column of the prokaryotes.txt, one id per line
 GCA_009840555.1
 GCA_009840575.1
 
-=====================================================================================================
+# You may want to get the id of genomes from a specific taxon using:
+https://www.ncbi.nlm.nih.gov/genome/browse#!/prokaryotes/refseq_category:reference
+
+============================================================================================================
 '''
 
 
 def genome_download_worker(argument_list):
 
     genome_record_split =      argument_list[0]
-    downloaded_genome_folder = argument_list[1]
-    get_fna =                  argument_list[2]
-    get_faa =                  argument_list[3]
-    get_gbff =                 argument_list[4]
-    with_name =                argument_list[5]
+    col_index =                argument_list[1]
+    downloaded_genome_folder = argument_list[2]
+    get_fna =                  argument_list[3]
+    get_faa =                  argument_list[4]
+    get_gbff =                 argument_list[5]
+    with_name =                argument_list[6]
 
-    genome_name = genome_record_split[0][1:-1]
+    genome_name = genome_record_split[0]
     genome_name_no_space = '_'.join(genome_name.split(' '))
     if ('(' in genome_name_no_space) and (')' in genome_name_no_space):
         genome_name_no_space_no_parenthesis_tmp = genome_name_no_space.replace('(', '_')
         genome_name_no_space = genome_name_no_space_no_parenthesis_tmp.replace(')', '')
 
-
-    GenBank_FTP = genome_record_split[-2][1:-1]
+    GenBank_FTP = genome_record_split[col_index['FTP Path']]
+    assembly_id = genome_record_split[col_index['Assembly Accession']]
     GenBank_FTP_id = GenBank_FTP.strip().split('/')[-1]
-    assembly_id = genome_record_split[5]
 
     # prepare cmds
     fna_file =                  '%s_genomic.fna.gz'                     % (GenBank_FTP_id)
@@ -107,7 +110,7 @@ def genome_download_worker(argument_list):
 
 def download_GenBank_genome(args):
 
-    csv_file =          args['csv']
+    csv_file =          args['db']
     output_folder =     args['out']
     assembly_id_file =  args['id']
     get_fna =           args['fna']
@@ -115,12 +118,20 @@ def download_GenBank_genome(args):
     get_gbff =          args['gbff']
     with_name =         args['name']
     num_threads =       args['t']
+    force_overwrite =   args['force']
 
     time_format = '[%Y-%m-%d %H:%M:%S] '
 
     if (get_fna is False) and (get_faa is False) and (get_gbff is False):
         print(datetime.now().strftime(time_format) + 'Please specify at least one file type to download, program exited')
         exit()
+
+    if os.path.isdir(output_folder) is True:
+        if force_overwrite is True:
+            os.system('rm -r %s' % output_folder)
+        else:
+            print(datetime.now().strftime(time_format) + 'Specified genome directory detected, program exited!')
+            exit()
 
     os.mkdir(output_folder)
 
@@ -141,23 +152,26 @@ def download_GenBank_genome(args):
     genomes_in_csv = set()
     genomes_in_csv_no_version = set()
     list_for_multiple_arguments_download_worker = []
+    col_index = {}
     for genome_record in open(csv_file):
-        if not genome_record.startswith('#Organism Name'):
-            genome_record_split = genome_record.strip().split(',')
-            assembly_id = genome_record_split[5][1:-1]
-            assembly_id_no_version = genome_record_split[5][1:-1].split('.')[0]
+        genome_record_split = genome_record.strip().split('\t')
+        if genome_record.startswith('#Organism/Name'):
+            col_index = {key: i for i, key in enumerate(genome_record_split)}
+        else:
+            assembly_id = genome_record_split[col_index['Assembly Accession']]
+            assembly_id_no_version = assembly_id.split('.')[0]
             if assembly_id_file is None:
-                list_for_multiple_arguments_download_worker.append([genome_record_split, output_folder, get_fna, get_faa, get_gbff, with_name])
+                list_for_multiple_arguments_download_worker.append([genome_record_split, col_index, output_folder, get_fna, get_faa, get_gbff, with_name])
                 genomes_in_csv.add(assembly_id)
                 genomes_in_csv_no_version.add(assembly_id.split('.')[0])
             else:
                 if assembly_id in assembly_id_set:
-                    list_for_multiple_arguments_download_worker.append([genome_record_split, output_folder, get_fna, get_faa, get_gbff, with_name])
+                    list_for_multiple_arguments_download_worker.append([genome_record_split, col_index, output_folder, get_fna, get_faa, get_gbff, with_name])
                 elif assembly_id_no_version in assembly_id_set_no_version:
-                    list_for_multiple_arguments_download_worker.append([genome_record_split, output_folder, get_fna, get_faa, get_gbff, with_name])
+                    list_for_multiple_arguments_download_worker.append([genome_record_split, col_index, output_folder, get_fna, get_faa, get_gbff, with_name])
                     different_version_found_dict[assembly_id_no_version] = assembly_id
 
-    # run COG annotaion files with multiprocessing
+    # run with multiprocessing
     pool = mp.Pool(processes=num_threads)
     pool.map(genome_download_worker, list_for_multiple_arguments_download_worker)
     pool.close()
@@ -205,14 +219,22 @@ def download_GenBank_genome(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-csv',         required=True,                       help='csv file downloaded from NCBI genome_browse')
+    parser.add_argument('-db',          required=True,                       help='local path to prokaryotes.txt')
     parser.add_argument('-out',         required=True,                       help='output folder')
-    parser.add_argument('-id',          required=False, default=None,        help='assembly id (6th col in the csv file), one id per line')
+    parser.add_argument('-id',          required=False, default=None,        help='IDs of genomes to download')
     parser.add_argument('-fna',         required=False, action="store_true", help='download fna file')
     parser.add_argument('-faa',         required=False, action="store_true", help='download faa file')
     parser.add_argument('-gbff',        required=False, action="store_true", help='download gbff file')
     parser.add_argument('-name',        required=False, action="store_true", help='include genome name in the downloaded files')
+    parser.add_argument('-force',       required=False, action="store_true", help='force overwrite existing genome directory')
     parser.add_argument('-t',           required=False, default=1, type=int, help='number of threads')
     args = vars(parser.parse_args())
     download_GenBank_genome(args)
 
+'''
+BioSAK get_genome_NCBI -csv /Users/songweizhi/DB/NCBI/prokaryotes-2022-05-18.txt -out gnm_dir -fna -t 10
+python3 ~/PycharmProjects/BioSAK/BioSAK/get_genome_NCBI.py -csv /Users/songweizhi/DB/NCBI/prokaryotes-2022-05-18.txt -out gnm_dir -fna -t 10
+
+python3 ~/PycharmProjects/BioSAK/BioSAK/get_genome_NCBI.py -db /Users/songweizhi/DB/NCBI/prokaryotes-2022-05-18.txt -id prokaryotes_to_download.txt -out gnm_dir -fna -t 10
+
+'''
