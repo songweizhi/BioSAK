eb358641a23726baa20f0cc5fafeca5f08cddcdb
Weizhi Song <songwz03@gmail.com>
2024-09-09 11:02:06 +0800

    -

--- BioSAK/sra.py
+++ BioSAK/sra.py
@@ -20,6 +20,9 @@ def sra(args):
     num_threads         = args['t']
     force_create_op_dir = args['f']
 
+    fasterq_dump_tmp    = '%s/fasterq_dump_tmp' % op_dir
+    cmd_txt             = '%s/cmds.txt'         % op_dir
+
     # create output folder
     if os.path.isdir(op_dir) is True:
         if force_create_op_dir is True:
@@ -28,6 +31,7 @@ def sra(args):
             print('Output folder detected, program exited!')
             exit()
     os.system('mkdir %s' % op_dir)
+    os.system('mkdir %s' % fasterq_dump_tmp)
 
     id_set = set()
     for each_id in open(id_txt):
@@ -36,10 +40,18 @@ def sra(args):
     prefetch_cmd_list = []
     fasterq_dump_cmd_list = []
     for each_id in id_set:
-        prefetch_cmd     = 'prefetch %s -O %s'              % (each_id, op_dir)
-        fasterq_dump_cmd = 'fasterq-dump %s --split-files'  % each_id
+        prefetch_cmd     = 'prefetch %s -O %s'                            % (each_id, op_dir)
+        fasterq_dump_cmd = 'fasterq-dump %s/%s --split-files -O %s -t %s' % (op_dir, each_id, op_dir, fasterq_dump_tmp)
         prefetch_cmd_list.append(prefetch_cmd)
         fasterq_dump_cmd_list.append(fasterq_dump_cmd)
+
+    # write out commands
+    cmd_txt_handle = open(cmd_txt, 'w')
+    for each in sorted(prefetch_cmd_list):
+        cmd_txt_handle.write(each + '\n')
+    for each in sorted(fasterq_dump_cmd_list):
+        cmd_txt_handle.write(each + '\n')
+    cmd_txt_handle.close()
 
     # download files with multiprocessing
     pool = mp.Pool(processes=num_threads)
@@ -48,7 +60,6 @@ def sra(args):
     pool.join()
 
     # extract fastq file from SRA with multiprocessing
-    os.chdir(op_dir)
     pool = mp.Pool(processes=num_threads)
     pool.map(os.system, fasterq_dump_cmd_list)
     pool.close()
